3c7fb53036e8a82cc5ae09309d707c6352828f10
Nestor <nlopezm@devco.com.co>
2022-10-21 16:28:13 -0500

    Rama Main

---
+++ src/main/java/com/devco/neslo/todolist/TodoListApp.java
@@ -0,0 +1,13 @@
+package com.devco.neslo.todolist;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListApp {
+    public static void main(String[] args) {
+        SpringApplication.run(TodoListApp.class,args);
+
+    }
+}
