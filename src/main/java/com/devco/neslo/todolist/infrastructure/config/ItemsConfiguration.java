2d8cf5766208b065501d256d704fede8fed12208
Nestor <nlopezm@devco.com.co>
2022-10-29 18:38:42 -0500

    ItemsController

---
+++ src/main/java/com/devco/neslo/todolist/infrastructure/config/ItemsConfiguration.java
@@ -0,0 +1,22 @@
+package com.devco.neslo.todolist.infrastructure.config;
+
+import com.devco.neslo.todolist.domain.items.ItemMediator;
+import com.devco.neslo.todolist.domain.items.ItemMediatorDefault;
+import com.devco.neslo.todolist.domain.persistence.ItemRepository;
+import com.devco.neslo.todolist.infrastructure.persistence.FakeItemRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ItemsConfiguration {
+
+    @Bean
+    public ItemRepository providesItemRepositoryInstance() {
+        return new FakeItemRepository();
+    }
+
+    @Bean
+    public ItemMediator providesItemMediatorInstance(ItemRepository repository) {
+        return new ItemMediatorDefault(repository);
+    }
+}
--- src/main/java/com/devco/neslo/todolist/infrastructure/controllers/ItemsController.java
+++ src/main/java/com/devco/neslo/todolist/infrastructure/controllers/ItemsController.java
@@ -4,6 +4,7 @@ import com.devco.neslo.todolist.domain.items.ItemMediator;
 import com.devco.neslo.todolist.domain.model.Item;
 import com.devco.neslo.todolist.infrastructure.mappers.ItemMapper;
 import com.devco.neslo.todolist.infrastructure.model.ItemDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,16 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ItemsController {
 
-    private final ItemMediator itemMediatorr;
 
-    public ItemsController(ItemMediator itemMediatorr) {
-        this.itemMediatorr = itemMediatorr;
+    private final ItemMediator itemMediator;
+
+    public ItemsController(ItemMediator itemMediator) {
+        this.itemMediator = itemMediator;
     }
-
     @PostMapping(path = "/lists/{listId}/items")
     public ResponseEntity<ItemDTO> create(@PathVariable("listId") long listId, @RequestBody ItemDTO itemDTO) {
         Item itemToCreate = ItemMapper.toItem(itemDTO);
-        Item itemCreated = itemMediatorr.create(listId, itemToCreate);
+        Item itemCreated = itemMediator.create(listId, itemToCreate);
         ItemDTO itemDTOCreated = ItemMapper.toItemDTO(itemCreated);
         return new ResponseEntity<>(itemDTOCreated, HttpStatus.CREATED);
     }
